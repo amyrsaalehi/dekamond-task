c45d3d041b24f2ae0f5c9f964ecfc7611e81a05a
Amir Salehi <salehi.amir9@gamil.com>
2025-08-30 19:44:10 +0530

    mock data added

---
+++ src/mock/user.ts
@@ -0,0 +1,56 @@
+export const mockUser = {
+  gender: "male",
+  name: {
+    title: "Mr",
+    first: "Manuel",
+    last: "Barela",
+  },
+  location: {
+    street: {
+      number: 5317,
+      name: "Corredor Sevilla",
+    },
+    city: "Nazareno",
+    state: "Veracruz",
+    country: "Mexico",
+    postcode: 70691,
+    coordinates: {
+      latitude: "24.7868",
+      longitude: "-95.6007",
+    },
+    timezone: {
+      offset: "-11:00",
+      description: "Midway Island, Samoa",
+    },
+  },
+  email: "manuel.barela@example.com",
+  login: {
+    uuid: "a10888ea-cba2-4b5f-aa0e-73416edfd0c2",
+    username: "beautifultiger499",
+    password: "pakistan",
+    salt: "XUNjTx0N",
+    md5: "1f113dff14042f7ee200364b5f9d7593",
+    sha1: "f87173bbd01a0e8f65bae6379d44029aaa85fcbc",
+    sha256: "ec134ead8ab65b0c068f316c9b91a024f974464e697d2736ff1a89bf89e5ca31",
+  },
+  dob: {
+    date: "1956-07-31T18:44:49.390Z",
+    age: 69,
+  },
+  registered: {
+    date: "2008-02-05T17:45:56.014Z",
+    age: 17,
+  },
+  phone: "(638) 898 0811",
+  cell: "(632) 571 7334",
+  id: {
+    name: "NSS",
+    value: "97 36 33 4879 1",
+  },
+  picture: {
+    large: "https://randomuser.me/api/portraits/men/8.jpg",
+    medium: "https://randomuser.me/api/portraits/med/men/8.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/men/8.jpg",
+  },
+  nat: "MX",
+};
